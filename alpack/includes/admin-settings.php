7cfc42b72c8a51806ac323031a4d74f5d58c3b06
jiji15899-arch <jiji15899@gmail.com>
2026-01-17 17:17:36 +0900

    Update admin-settings.php

--- alpack/includes/admin-settings.php
+++ alpack/includes/admin-settings.php
@@ -5,7 +5,7 @@ add_action('admin_menu', function() {
     add_menu_page('ALPACK AI 설정', 'ALPACK AI', 'manage_options', 'presslearn-settings', 'pl_ai_render_settings_page', 'dashicons-admin-generic');
 });
 
-function pl_ai_render_settings_page() {
+function render_ai_settings_page() {
     if (isset($_POST['pl_save'])) {
         update_option('pl_ai_base_lang', $_POST['pl_base_lang']);
         echo '<div class="updated"><p>설정이 저장되었습니다.</p></div>';
